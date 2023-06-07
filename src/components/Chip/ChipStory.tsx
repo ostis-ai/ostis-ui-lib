f68792a908aa84b38009d9c4e7fa583fb1f0e1d1
alena cherkas <acherkas@gomel.iba.by>
2023-06-07 11:19:40 +0300

    feat: add chip component

---
+++ src/components/Chip/ChipStory.tsx
@@ -0,0 +1,9 @@
+import { StoryItem } from '@components/Storybook/StoryItem';
+
+import { Chip } from './Chip';
+
+export const ChipStory = () => (
+  <StoryItem path="Chip">
+    <Chip>Chip</Chip>
+  </StoryItem>
+);
--- src/index.tsx
+++ src/index.tsx
@@ -2,5 +2,6 @@ export * from '@components/SwitchScgScn';
 export * from '@components/Tooltip';
 export * from '@components/Toast';
 export * from '@components/Toasts';
+export * from '@components/Chip';
 export * from '@components/Spinner';
 export * from '@components/PositionInPortal';
