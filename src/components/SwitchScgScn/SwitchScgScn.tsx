6e1adc7a911ebd826543b340d21003bc805f619a
Vladislav Kovalev <78146509+Kovallevz@users.noreply.github.com>
2023-06-26 13:54:49 +0300

    refactor: tooltip and switchScgScn style transfer

--- src/components/SwitchScgScn/SwitchScgScn.tsx
+++ src/components/SwitchScgScn/SwitchScgScn.tsx
@@ -1,62 +1,10 @@
 import { Tooltip } from '@components/Tooltip';
-import styled, { css } from 'styled-components';
 
 import ScgSwitch from './icons/scg.svg';
 import ScnSwitch from './icons/scn.svg';
+import { Divider, SwitchWrap, Tab, Tabs } from './styled';
 
 export type TScLanguageTab = 'scn' | 'scg';
-
-const SwitchWrap = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-`;
-
-const Tabs = styled.div`
-  display: flex;
-  justify-content: center;
-
-  width: 100%;
-`;
-
-const Tab = styled.div<{ isActive: boolean }>`
-  font-size: 26px;
-  font-weight: 400;
-  color: #c0c0c0;
-
-  cursor: pointer;
-
-  &:hover {
-    background: #e9f3f6;
-    path {
-      fill: #5896c0;
-    }
-  }
-
-  padding: 4px 8px;
-
-  border-radius: 4px;
-
-  path {
-    fill: #c0c0c0;
-  }
-
-  ${(props) =>
-    props.isActive &&
-    css`
-      background: #f8f8f8;
-
-      path {
-        fill: #5896c0;
-      }
-    `}
-`;
-
-const Divider = styled.span`
-  margin: 0 4px;
-
-  border-left: 1px solid #c0c0c0;
-`;
 
 export interface ISwitchScgScnProps {
   className?: string;
---
+++ src/components/SwitchScgScn/styled.ts
@@ -0,0 +1,53 @@
+import styled, { css } from 'styled-components';
+
+export const SwitchWrap = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Tabs = styled.div`
+  display: flex;
+  justify-content: center;
+
+  width: 100%;
+`;
+
+export const Tab = styled.div<{ isActive: boolean }>`
+  font-size: 26px;
+  font-weight: 400;
+  color: #c0c0c0;
+
+  cursor: pointer;
+
+  &:hover {
+    background: #e9f3f6;
+    path {
+      fill: #5896c0;
+    }
+  }
+
+  padding: 4px 8px;
+
+  border-radius: 4px;
+
+  path {
+    fill: #c0c0c0;
+  }
+
+  ${(props) =>
+    props.isActive &&
+    css`
+      background: #f8f8f8;
+
+      path {
+        fill: #5896c0;
+      }
+    `}
+`;
+
+export const Divider = styled.span`
+  margin: 0 4px;
+
+  border-left: 1px solid #c0c0c0;
+`;
