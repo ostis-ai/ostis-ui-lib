5651f5f89c68b45c28bd9b4bf02f5eb213ee9178
Гриневич Александр Васильевич <agrinevich@t1.ru>
2023-10-24 11:49:19 +0300

    feat: deprecate inputv2

--- src/StorybookInit.tsx
+++ src/StorybookInit.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable simple-import-sort/imports */
 import { ButtonStory } from '@components/Button/ButtonStory';
 import { ChipStory } from '@components/Chip/ChipStory';
 import { DropdownStory } from '@components/Dropdown/DropdownStory';
@@ -6,16 +5,15 @@ import { InputStory } from '@components/Input/InputStory';
 import { InputV2Story } from '@components/InputV2/InputV2Story';
 import { NotificationStory } from '@components/Notification/NotificationStory';
 import { PopupStory } from '@components/Popup/PopupStory';
+import { ScnStory } from '@components/Scn/ScnStory';
 import { SkeletonStory } from '@components/Skeleton/story/SkeletonStory';
 import { SpinnerStory } from '@components/Spinner/SpinnerStory';
 import { Storybook } from '@components/Storybook';
 import { SwitchScgScnStory } from '@components/SwitchScgScn/SwitchScgScnStory';
 import { TextareaStory } from '@components/Textarea/TextareaStory';
 import { TooltipStory } from '@components/Tooltip/TooltipStory';
-import { ThemeProvider, createGlobalStyle } from 'styled-components';
-
-import { ScnStory } from '@components/Scn/ScnStory';
 import { defaultLight } from '@constants/theme';
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
 
 const Global = createGlobalStyle`
   body {
